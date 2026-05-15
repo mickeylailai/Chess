a9d41c395c18748923f10ab0d2605d96b441e28b
mickeylailai <mickeylaimickey@gmail.com>
2026-05-15 10:04:30 +0800

    Reapply "最終版"
    This reverts commit 714ea72d2e995284f1c181625714de29cff2db99.

--- Fen_getting.py
+++ Fen_getting.py
@@ -4,13 +4,13 @@ import pyperclip
 def start_listening(previous_fen=""):
     while True:
         
-        # 讀取剪貼簿中的文字
+        # 讀剪貼簿
         current_text = pyperclip.paste().strip()
         
-        # 如果格式符合 Fen (有7個 '/') 且不是上一次的 Fen
+        # 新 FEN 才回傳
         if current_text.count('/') == 7 and current_text != previous_fen:
             print(f"\n[獲取新 FEN]: {current_text}")
             return current_text
             
-        # 暫停 0.5 秒，避免 CPU 佔用率過高
+        # 降 CPU
         time.sleep(0.5)
--- zobrist.py
+++ zobrist.py
@@ -1,15 +1,15 @@
 import random
 
-#12 種棋子 * 64 格
+# 12 種棋子 * 64 格
 PIECES = [[random.getrandbits(64) for _ in range(64)] for _ in range(12)]
 
-#黑方
+# 黑方回合
 BLACK_TO_MOVE = random.getrandbits(64)
 
-#入堡
+# 入堡權
 CASTLING = [random.getrandbits(64) for _ in range(4)]
 
-#過路兵
+# 過路兵欄位
 EN_PASSANT = [random.getrandbits(64) for _ in range(8)]
 
 def piece_to_index(piece):
